95a81bf15274c7b1775f30db707513c23f24463b
Paul Hekkema <paul@hekkema.nl>
2017-10-28 14:54:23 +0200

    Setting up bundle

---
+++ QueryBrowserBundle.php
@@ -0,0 +1,20 @@
+/**
+ * QueryBrowserBundle
+ *
+ * @link      https://gitlab.kapma.nl/paulhekkema/QueryBrowserBundle
+ * @license   MIT (see LICENSE for details)
+ * @author    Paul Hekkema <paul@hekkema.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace PaulHekkema\QueryBrowserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class QueryBrowserBundle extends Bundle
+{
+}
